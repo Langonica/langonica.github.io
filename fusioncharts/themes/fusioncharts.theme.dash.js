06bd6d3253da92f4d58b1361120bbc7f9f9205c9
Langonica <langonica@gmail.com>
2014-12-23 11:32:10 -0500

    temp

--- fusioncharts/themes/fusioncharts.theme.dash.js
+++ fusioncharts/themes/fusioncharts.theme.dash.js
@@ -434,6 +434,7 @@ FusionCharts.register('theme', {
             chart: {
                 plotFillColor: '#04476c',
                 targetColor: '#4d998d',
+                showTickMarks: '0';
                 lowerLimit: '-20',
       			upperLimit: '120'
             }
