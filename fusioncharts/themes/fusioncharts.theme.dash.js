b1ac72254c21f7b466873b840cc9347973280740
Langonica <langonica@gmail.com>
2014-12-23 11:13:07 -0500

    therm

--- fusioncharts/themes/fusioncharts.theme.dash.js
+++ fusioncharts/themes/fusioncharts.theme.dash.js
@@ -386,6 +386,7 @@ FusionCharts.register('theme', {
       			decimals: '1',
       			numberSuffix: '°F',
       			showhovereffect: '0',
+      			thmBulbRadius: "40",
       			thmHeight: '65',
       			showGaugeBorder: '1',
       			gaugeBorderColor: '#008ee4',
